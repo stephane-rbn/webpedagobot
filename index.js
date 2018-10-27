6593b4a4ca8654b3706e0c12eb68e93282526d86
Stéphane <rabena.stephane@gmail.com>
2018-10-28 01:08:55 +0200

    fix(index.js): check environment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const SlackBot = require('slackbots');
 const axios = require('axios');
